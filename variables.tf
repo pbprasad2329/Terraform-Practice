aef1c79679df09108efff1e6abce9716a4a28769
pbprasad2329 <118789941+pbprasad2329@users.noreply.github.com>
2023-10-21 08:38:25 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -39,7 +39,7 @@ variable "acl" {
 variable "bucket_prefix" {
  type        = string
  description = "(required since we are not using 'bucket') Creates a unique bucket name beginning with the specified prefix"
- default     = "my-ddrs-bucket"
+ default     = "my-prasad2329-bucket"
 }
 variable "tags" {
  type        = map
