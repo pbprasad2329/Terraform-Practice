79a7321368f7d95b160a8e479e5518d72538367a
pbprasad2329 <118789941+pbprasad2329@users.noreply.github.com>
2023-10-21 10:44:52 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -39,7 +39,7 @@ variable "acl" {
 variable "bucket_prefix" {
  type        = string
  description = "(required since we are not using 'bucket') Creates a unique bucket name beginning with the specified prefix"
- default     = "my-prasad2329-bucket"
+ default     = "demo-cls"
 }
 variable "tags" {
  type        = map
